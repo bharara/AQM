5d000a9e27fedd592d75ef1c783c36204036c8c5
bharara <32545366+bharara@users.noreply.github.com>
2018-12-14 08:47:16 +0500

    To Read

--- sensor-Schema.sql
+++ sensor-Schema.sql
@@ -2,28 +2,28 @@ DROP SCHEMA IF EXISTS sensor;
 CREATE SCHEMA sensor;
 USE sensor;
 
-
-use sensor;
+grant all privileges on sensor.* to 'root'@localhost;  
 
 create table data (
-	idData int auto_increment primary key not null,
+    idData int auto_increment primary key not null,
     val1 int,
     val2 int);
 
 create table classroom (
-	classRoom varchar(30) primary key not null,
+    classRoom varchar(30) primary key not null,
     openings int,
     floorplan int);
     
 create table readings (
-	classRoom varchar(30),
+    classRoom varchar(30),
     startInstance int,
     endInstance int,
     people int);
     
 create table admins (
-	adminName varchar(80) primary key,
+    adminName varchar(80) primary key,
     pword varchar(80) unique);
     
 insert into classroom values ('cr8',3,2);
 insert into admins values ('hadi','1234');
+insert into readings values ('cr8',0,0,1);
